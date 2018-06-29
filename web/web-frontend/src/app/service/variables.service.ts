fb06fbe0b11059efb69535cb13d395193a6f00d8
Ionut Pruteanu <ionut.pruteanu@gmail.com>
2018-06-29 12:44:10 +0200

    work on removing deprecated HTTP client and to use the new error handling

--- web/web-frontend/src/app/service/variables.service.ts
+++ web/web-frontend/src/app/service/variables.service.ts
@@ -15,56 +15,46 @@ import {ValidationErrorResponse} from "../model/exception/validation-error-respo
 import {StepsService} from "./steps.service";
 import {JsonUtil} from "../utils/json.util";
 import {Subject} from "rxjs/Rx";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {FeatureService} from "./feature.service";
+import {RootServerTreeNode} from "../model/tree/root-server-tree-node.model";
+import {Feature} from "../model/feature/feature.model";
 
 @Injectable()
 export class VariablesService {
 
     private VARIABLES_URL = "/rest/variables";
 
-    constructor(private router: Router,
-                private http:Http,
-                private errorService: ErrorService) {
-    }
+    constructor(private http: HttpClient) {}
+
 
     getVariables(): Observable<Array<Variable>> {
         return this.http
-            .get(this.VARIABLES_URL)
-            .map(VariablesService.extractVariablesModel)
-            .catch(err => {return this.errorService.handleHttpResponseException(err)});
+            .get<Array<Variable>>(this.VARIABLES_URL)
+            .map(VariablesService.extractVariablesModel);
     }
 
-    private static extractVariablesModel(res: Response):Array<Variable> {
-        let json = res.json();
+    save(model: Array<Variable>): Observable<Array<Variable>> {
+        let body = JsonUtil.serializeArray(model);
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type':  'application/json',
+            })
+        };
 
+        return this.http
+            .post<Array<Variable>>(this.VARIABLES_URL, body, httpOptions)
+            .map(VariablesService.extractVariablesModel);
+    }
+
+    private static extractVariablesModel(res: Array<Variable>):Array<Variable> {
         let response:Array<Variable> = [];
-        for(let variableAsJson of json) {
+        for(let variableAsJson of res) {
             let testsModel = new Variable().deserialize(variableAsJson);
             response.push(testsModel)
         }
         response.push(new Variable());
 
         return response;
-    }
-
-    save(model: Array<Variable>): Observable<Array<Variable>> {
-        let body = JsonUtil.serializeArray(model);
-        let headers = new Headers({'Content-Type': 'application/json'});
-        let options = new RequestOptions({headers: headers});
-
-        let responseSubject: Subject<Array<Variable>> = new Subject<Array<Variable>>();
-        this.http
-            .post(this.VARIABLES_URL, body, options)
-            .subscribe(
-                (response: Response) => {
-                    responseSubject.next(
-                        VariablesService.extractVariablesModel(response)
-                    )
-                },
-                (err) => {
-                    this.errorService.handleHttpResponseException(err)
-                }
-            );
-
-        return responseSubject.asObservable();
     }
 }
