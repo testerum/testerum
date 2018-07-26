86b9b606049ed21b319e42df67cda4b75366429c
Cristian Mocanu <cvmocanu@gmail.com>
2018-07-26 14:12:25 +0200

    web-frontend/start.sh: show Linux notifications

--- web/web-frontend/start.sh
+++ web/web-frontend/start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-ng serve --proxy-config proxy-conf.json --live-reload false
+unbuffer ng serve --proxy-config proxy-conf.json --live-reload false \
+    | tee /dev/tty \
+    | gawk '/webpack: Compiled successfully./ { system("notify-send ng\\ serve Compiled\\ successfully." timeTaken " -i info --expire-time 2000") }
+           /ERROR in/ { system("notify-send ng\\ serve ERROR -i error --expire-time 999999999") }'
 
