76dd6f3e8ee2a81946919180da45dd5532ff8439
Cristian Mocanu <cvmocanu@gmail.com>
2018-08-01 15:34:04 +0200

    allow empty values in "WHEN I type <<text>> into the field <<elementLocator>>" step

--- steps/selenium-steps/src/main/kotlin/selenium/actions/WebDriverFormSteps.kt
+++ steps/selenium-steps/src/main/kotlin/selenium/actions/WebDriverFormSteps.kt
@@ -1,5 +1,6 @@
 package selenium.actions
 
+import com.testerum.api.annotations.steps.Param
 import com.testerum.api.annotations.steps.When
 import org.openqa.selenium.WebElement
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,14 +10,15 @@ import selenium_steps_support.service.webdriver_manager.WebDriverManager
 class WebDriverFormSteps @Autowired constructor(private val webDriverManager: WebDriverManager) {
 
     @When("I type <<text>> into the field <<elementLocator>>")
-    fun sendKeys(text: String, elementLocator: String) {
+    fun sendKeys(@Param(required = false) text: String?, elementLocator: String) {
         webDriverManager.executeWebDriverStep { driver ->
             val field: WebElement = ElementLocatorService.locateElement(driver, elementLocator)
                     ?: throw AssertionError("the field [$elementLocator] should be present on the page, but is not")
 
             // todo: parse "text" and send org.openqa.selenium.Keys if needed
 
-            field.sendKeys(text)
+            field.clear()
+            field.sendKeys(text.orEmpty())
         }
     }
 
