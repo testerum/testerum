35baa847eeb585be5b336426602413286c286b43
Cristian Mocanu <cvmocanu@gmail.com>
2018-08-02 14:46:30 +0200

    fix start.sh script after migration to Angular 6

--- web/web-frontend/start.sh
+++ web/web-frontend/start.sh
@@ -2,6 +2,6 @@
 
 unbuffer ng serve --proxy-config proxy-conf.json --live-reload false \
     | tee /dev/tty \
-    | gawk '/webpack: Compiled successfully./ { system("notify-send ng\\ serve Compiled\\ successfully." timeTaken " -i info --expire-time 2000") }
+    | gawk '/Compiled successfully./ { system("notify-send ng\\ serve Compiled\\ successfully." timeTaken " -i info --expire-time 2000") }
            /ERROR in/ { system("notify-send ng\\ serve ERROR -i error --expire-time 999999999") }'
 
