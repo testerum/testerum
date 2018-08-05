0dfb0871c63cf56a99b684849da50b0299694988
Cristian Mocanu <cvmocanu@gmail.com>
2018-08-05 15:43:30 +0200

    frontend: implement Hot Module Replacement

--- web/web-frontend/start.sh
+++ web/web-frontend/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-unbuffer ng serve --proxy-config proxy-conf.json --live-reload false \
+unbuffer ng serve --proxy-config proxy-conf.json --configuration hmr \
     | tee /dev/tty \
     | gawk '/Compiled successfully./ { system("notify-send ng\\ serve Compiled\\ successfully." timeTaken " -i info --expire-time 2000") }
            /ERROR in/ { system("notify-send ng\\ serve ERROR -i error --expire-time 999999999") }'
