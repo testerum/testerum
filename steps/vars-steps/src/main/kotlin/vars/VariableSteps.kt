8c69143195f77444ca885d5be29867c90643c46e
Cristian Mocanu <cvmocanu@gmail.com>
2018-08-13 19:01:28 +0200

    TM-146 Steps not properly configured

--- steps/vars-steps/src/main/kotlin/vars/VariableSteps.kt
+++ steps/vars-steps/src/main/kotlin/vars/VariableSteps.kt
@@ -2,10 +2,12 @@ package vars
 
 import com.testerum.api.annotations.steps.Given
 import com.testerum.api.annotations.steps.Param
+import com.testerum.api.services.TesterumServiceLocator
 import com.testerum.api.test_context.test_vars.TestVariables
-import org.springframework.beans.factory.annotation.Autowired
 
-class VariableSteps @Autowired constructor(private val variables: TestVariables) {
+class VariableSteps {
+
+    private val variables: TestVariables = TesterumServiceLocator.getTestVariables()
 
     @Given("the variable <<name>> with value <<value>>")
     fun declareVariable(name: String,
