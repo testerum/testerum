c918d5f4fe12661c4acfba57dcb1e435da62cfc5
Cristian Mocanu <cvmocanu@gmail.com>
2018-08-14 12:18:09 +0200

    prepare-package.sh: include "var-steps"

--- prepare-package.sh
+++ prepare-package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mvn package -T 2C -Dmaven.test.skip=true -am -pl steps/assertion-steps/,steps/http-steps/,steps/rdbms-steps/,steps/selenium-steps/,testerum-backend/testerum-runner/testerum-runner-cmdline
+mvn package -T 2C -Dmaven.test.skip=true -am -pl steps/assertion-steps/,steps/http-steps/,steps/rdbms-steps/,steps/selenium-steps/,steps/vars-steps/,testerum-backend/testerum-runner/testerum-runner-cmdline
 
