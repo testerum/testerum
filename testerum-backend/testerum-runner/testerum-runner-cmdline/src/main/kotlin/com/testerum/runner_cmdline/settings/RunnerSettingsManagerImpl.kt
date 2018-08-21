181f5ec4902a3da9accc2eec5bb8fb0dd9cc4770
Cristian Mocanu <cvmocanu@gmail.com>
2018-08-21 17:07:44 +0200

    fix build

--- testerum-backend/testerum-runner/testerum-runner-cmdline/src/main/kotlin/com/testerum/runner_cmdline/settings/RunnerSettingsManagerImpl.kt
+++ testerum-backend/testerum-runner/testerum-runner-cmdline/src/main/kotlin/com/testerum/runner_cmdline/settings/RunnerSettingsManagerImpl.kt
@@ -7,8 +7,6 @@ import com.testerum.settings.getRequiredSetting
 
 class RunnerSettingsManagerImpl(private val settingsManager: SettingsManager) : RunnerSettingsManager {
 
-    override fun getSettings(): List<Setting> = settingsManager.getSettings()
-
     override fun getSetting(key: String): Setting? = settingsManager.getSetting(key)
 
     override fun getRequiredSetting(key: String): Setting = settingsManager.getRequiredSetting(key)
