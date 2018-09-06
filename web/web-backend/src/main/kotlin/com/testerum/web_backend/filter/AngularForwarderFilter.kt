38f272995119aa95ca054810651d5fdf28345dc6
Cristian Mocanu <cvmocanu@gmail.com>
2018-09-06 12:31:12 +0200

    web-backend: serving index.html instead of static files
    TM-31 backend: nice URLs don't work server-side

--- web/web-backend/src/main/kotlin/com/testerum/web_backend/filter/AngularForwarderFilter.kt
+++ web/web-backend/src/main/kotlin/com/testerum/web_backend/filter/AngularForwarderFilter.kt
@@ -4,18 +4,12 @@ import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class ForwarderFilter : Filter {
-
-    companion object {
-        private val SPLITTER = Regex("\\s+")
-    }
+class AngularForwarderFilter : Filter {
 
     private lateinit var forwardToUrl: String
-    private lateinit var ignoredUrls: List<Regex>
 
     override fun init(config: FilterConfig) {
-        forwardToUrl = config.getForwardToUrl()
-        ignoredUrls = config.getIgnoredUrls()
+        forwardToUrl = config.getParameter("forwardToUrl")
     }
 
     override fun destroy() {}
@@ -35,25 +29,14 @@ class ForwarderFilter : Filter {
                              chain: FilterChain) {
         val requestURI = request.requestURI
 
-        if (ignoredUrls.matches(requestURI)) {
-            chain.doFilter(request, response)
-        } else {
+        if (AngularForwardingSelector.shouldForward(requestURI)) {
             val requestDispatcher = request.getRequestDispatcher(forwardToUrl)
             requestDispatcher.forward(request, response)
+        } else {
+            chain.doFilter(request, response)
         }
     }
 
-    private fun List<Regex>.matches(text: String): Boolean = this.any { it.matches(text) }
-
-    private fun FilterConfig.getForwardToUrl(): String = getParameter("forwardToUrl")
-
-    private fun FilterConfig.getIgnoredUrls(): List<Regex> {
-        val paramValue = getParameter("ignoredUrls")
-
-        return SPLITTER.split(paramValue)
-                .filter { it.isNotBlank() }
-                .map { Regex(it) }
-    }
 
     private fun FilterConfig.getParameter(name: String): String {
         return this.getInitParameter(name)
