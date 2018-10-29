66205399aee8f65fd6c54509c150218f44c47a19
Cristian Mocanu <cvmocanu@gmail.com>
2018-10-29 12:50:32 +0100

    rename log configuration file
    TM-381 gui launcher: log to file

--- web/web-launcher/src/main/kotlin/com/testerum/launcher/config/PathsManager.kt
+++ web/web-launcher/src/main/kotlin/com/testerum/launcher/config/PathsManager.kt
@@ -20,7 +20,7 @@ object PathsManager {
 
     val configFilePath: JavaPath = settingsDir.resolve("testerum-launcher.properties").toAbsolutePath().normalize()
 
-    val logConfigFilePath: JavaPath = testerumRootDir.resolve("conf/logback.xml").toAbsolutePath().normalize()
+    val logConfigFilePath: JavaPath = testerumRootDir.resolve("conf/testerum-logback.xml").toAbsolutePath().normalize()
 
     val classpathRepoDir: JavaPath = testerumRootDir.resolve("repo").toAbsolutePath().normalize()
 }
