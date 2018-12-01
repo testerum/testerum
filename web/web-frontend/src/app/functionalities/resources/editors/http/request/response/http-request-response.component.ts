5e743fe36b92b3ea4a45829faa7e52588edd55ab
Cristian Mocanu <cvmocanu@gmail.com>
2018-12-01 19:25:10 +0100

    frontend: http-request-response: improve looks

--- web/web-frontend/src/app/functionalities/resources/editors/http/request/response/http-request-response.component.ts
+++ web/web-frontend/src/app/functionalities/resources/editors/http/request/response/http-request-response.component.ts
@@ -4,7 +4,8 @@ import {HttpRequestService} from "../http-request.service";
 @Component({
     moduleId: module.id,
     selector: 'http-request-response',
-    templateUrl: 'http-request-response.component.html'
+    templateUrl: 'http-request-response.component.html',
+    styleUrls: ['http-request-response.component.scss']
 })
 
 export class HttpResponseComponent implements OnInit {
