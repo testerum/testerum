056a21a46fc749780caca39a3c983306bbc53d74
Cristian Mocanu <cvmocanu@gmail.com>
2018-12-13 15:49:31 +0100

    http steps: pretty-print JSON HTTP request and response bodies

--- model/src/main/java/com/testerum/model/resources/http/request/HttpRequest.kt
+++ model/src/main/java/com/testerum/model/resources/http/request/HttpRequest.kt
@@ -22,24 +22,5 @@ data class HttpRequest @JsonCreator constructor(
     fun getContentTypeHeaderValue(): String?
             = getFirstHeaderValue("Content-Type")
 
-    override fun toString(): String {
-        var response = "$method $url\n"
-
-        for ((headerName, headerValue) in headers) {
-            response += "$headerName: $headerValue\n"
-        }
-
-        if (body?.bodyType != null) {
-            response += "\n"
-            response += "Body type: ${body.bodyType}\n"
-        }
-
-        if (body?.content?.isNotEmpty() == true) {
-            response +=  "\n"
-            response += body.content
-        }
-
-        return response
-    }
 
 }
