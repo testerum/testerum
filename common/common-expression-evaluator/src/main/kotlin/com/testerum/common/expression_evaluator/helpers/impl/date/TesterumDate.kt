25b2a8b1495c1b1d66bbd5bbc0270d0703854cea
Cristian Mocanu <cvmocanu@gmail.com>
2019-01-06 17:38:46 +0100

    TM-574 Helper Functions

--- common/common-expression-evaluator/src/main/kotlin/com/testerum/common/expression_evaluator/helpers/impl/date/TesterumDate.kt
+++ common/common-expression-evaluator/src/main/kotlin/com/testerum/common/expression_evaluator/helpers/impl/date/TesterumDate.kt
@@ -5,79 +5,120 @@ import java.time.format.DateTimeFormatter
 
 class TesterumDate {
 
-    private val date: LocalDateTime = LocalDateTime.now();
-    private var formatter: DateTimeFormatter? = null;
+    private val date: LocalDateTime
+
+    constructor(date: LocalDateTime) {
+        this.date = date
+    }
+
+    constructor() {
+        date = LocalDateTime.now()
+    }
+
+    constructor(year: Int, month: Int, dayOfMonth: Int) {
+        date = LocalDateTime.of(year, month, dayOfMonth, 0, 0, 0)
+    }
+
+    constructor(year: Int, month: Int, dayOfMonth: Int, hour: Int, minute: Int, second: Int) {
+        date = LocalDateTime.of(year, month, dayOfMonth, hour, minute, second)
+    }
+
+    constructor(year: Int, month: Int, dayOfMonth: Int, hour: Int, minute: Int, second: Int, nanoOfSecond: Int) {
+        date = LocalDateTime.of(year, month, dayOfMonth, hour, minute, second, nanoOfSecond)
+    }
 
     fun minusYears(years: Long): TesterumDate {
-        date.minusYears(years)
-        return this;
-    }
-
-    fun minusMonths(months: Long): TesterumDate {
-        date.minusMonths(months)
-        return this;
-    }
-
-    fun minusWeeks(weeks: Long): TesterumDate {
-        date.minusWeeks(weeks)
-        return this;
-    }
-
-    fun minusHours(hours: Long): TesterumDate {
-        date.minusHours(hours)
-        return this;
-    }
-
-    fun minusMinutes(minutes: Long): TesterumDate {
-        date.minusMinutes(minutes)
-        return this;
-    }
-
-    fun minusSeconds(seconds: Long): TesterumDate {
-        date.minusSeconds(seconds)
-        return this;
+        return TesterumDate(
+                date.minusYears(years)
+        )
     }
 
     fun plusYears(years: Long): TesterumDate {
-        date.plusYears(years)
-        return this;
+        return TesterumDate(
+                date.plusYears(years)
+        )
+    }
+
+
+    fun minusMonths(months: Long): TesterumDate {
+        return TesterumDate(
+                date.minusMonths(months)
+        )
     }
 
     fun plusMonths(months: Long): TesterumDate {
-        date.plusMonths(months)
-        return this;
+        return TesterumDate(
+                date.plusMonths(months)
+        )
+    }
+
+    fun minusWeeks(weeks: Long): TesterumDate {
+        return TesterumDate(
+                date.minusWeeks(weeks)
+        )
     }
 
     fun plusWeeks(weeks: Long): TesterumDate {
-        date.plusWeeks(weeks)
-        return this;
+        return TesterumDate(
+                date.plusWeeks(weeks)
+        )
+    }
+
+    fun minusHours(hours: Long): TesterumDate {
+        return TesterumDate(
+                date.minusHours(hours)
+        )
     }
 
     fun plusHours(hours: Long): TesterumDate {
-        date.plusHours(hours)
-        return this;
+        return TesterumDate(
+                date.plusHours(hours)
+        )
+    }
+
+    fun minusMinutes(minutes: Long): TesterumDate {
+        return TesterumDate(
+                date.minusMinutes(minutes)
+        )
     }
 
     fun plusMinutes(minutes: Long): TesterumDate {
-        date.plusMinutes(minutes)
-        return this;
+        return TesterumDate(
+                date.plusMinutes(minutes)
+        )
+    }
+
+    fun minusSeconds(seconds: Long): TesterumDate {
+        return TesterumDate(
+                date.minusSeconds(seconds)
+        )
     }
 
     fun plusSeconds(seconds: Long): TesterumDate {
-        date.plusSeconds(seconds)
-        return this;
+        return TesterumDate(
+                date.plusSeconds(seconds)
+        )
     }
 
-    fun format(format: String): TesterumDate {
-        this.formatter = DateTimeFormatter.ofPattern(format)
-        return this;
+    fun minusNanos(nanos: Long): TesterumDate {
+        return TesterumDate(
+                date.minusNanos(nanos)
+        )
+    }
+
+    fun plusNanos(nanos: Long): TesterumDate {
+        return TesterumDate(
+                date.plusNanos(nanos)
+        )
     }
 
     override fun toString(): String {
-        if (this.formatter == null) {
-            return date.toString()
-        }
+        return date.toString()
+    }
 
-        return date.format(formatter)
+    fun toString(format: String): String {
+        return date.format(
+                DateTimeFormatter.ofPattern(format)
+        )
     }
 }
