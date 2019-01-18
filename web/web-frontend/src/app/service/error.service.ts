6ef05d9641168bca958cf1286328d0b50046bd58
Ionut Pruteanu <ionut.pruteanu@gmail.com>
2019-01-18 18:53:15 +0100

    TM-522 UI: detect server down and show a nice overlay, like Angular does

--- web/web-frontend/src/app/service/error.service.ts
+++ web/web-frontend/src/app/service/error.service.ts
@@ -44,7 +44,7 @@ export class ErrorService implements HttpInterceptor {
                     if (httpErrorResponse.status >= 400 || httpErrorResponse.status == 0) {
 
                         if (err.status == 504 || err.status == 0) {
-                            if (httpErrorResponse.url && httpErrorResponse.url.endsWith(this.PING_REQUEST_PATH)) {
+                            if (!ErrorService.isServerAvailable) {
                                 return EMPTY;
                             }
                             this.utilService.checkIfServerIsAvailable().subscribe((isServerAvailable: boolean) => {
