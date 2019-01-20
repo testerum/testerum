efcd48187ca09761903968fd876818ddef83101d
Ionut Pruteanu <ionut.pruteanu@gmail.com>
2019-01-20 14:32:36 +0100

    work on statistics

--- testerum-backend/testerum-runner/testerum-runner-cmdline/src/main/assembly/report_templates/templates/stats/app/src/app/stats/line-stats/line-stats.component.ts
+++ testerum-backend/testerum-runner/testerum-runner-cmdline/src/main/assembly/report_templates/templates/stats/app/src/app/stats/line-stats/line-stats.component.ts
@@ -1,32 +1,33 @@
-import {Component, ElementRef, HostBinding, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostBinding, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {StatsService} from "../../service/stats.service";
 import {ExecutionStatus} from "../../../../../../../common/testerum-model/report-model/model/report/execution-status";
 import {DataPointModel} from "../../model/data-point.model";
-import {UIChart} from "primeng/chart";
 import {StatsType} from "../../model/stats-type.enum";
+import {ArrayUtil} from "../../../../../../pretty/app/src/app/util/array.util";
+import {UIChart} from "primeng/chart";
 
 @Component({
     selector: 'line-stats',
     templateUrl: './line-stats.component.html',
     styleUrls: ['./line-stats.component.scss'],
 })
-export class LineStatsComponent implements OnInit {
+export class LineStatsComponent implements OnInit, OnChanges {
 
     @Input() title: string;
     @Input() statsType: StatsType;
     @Input() tag: string;
     @Input()  widthInPercentage: number = 45;
 
+    @Input() startDate: Date;
+    @Input() endDate: Date;
+
+    @Input() showDetails: boolean;
+
     data: any;
     options: any;
 
     firstPossibleDate: Date;
     lastPossibleDate: Date;
-
-    firstDisplayedDate: Date;
-    lastDisplayedDate: Date;
-
-    rangeDates: Date[] = [];
 
     allPassedData: DataPointModel[] = [];
     allFailedData: DataPointModel[] = [];
@@ -82,18 +83,10 @@ export class LineStatsComponent implements OnInit {
         this.disabledData = this.allDisabledData;
         this.skippedData = this.allSkippedData;
 
-        if (this.passedData.length > 0) {
-            this.firstPossibleDate = this.passedData[0].x;
-            this.lastPossibleDate = this.passedData[this.passedData.length - 1].x;
+        this.firstPossibleDate = this.statsService.statsModelExtractor.getFirstDate();
+        this.lastPossibleDate = this.statsService.statsModelExtractor.getLastDate();
 
-            this.firstDisplayedDate = this.firstPossibleDate;
-            this.lastDisplayedDate = this.lastPossibleDate;
-
-            this.rangeDates.push(this.firstDisplayedDate);
-            this.rangeDates.push(this.lastDisplayedDate);
-        }
-
-        this.data = this.createData();
+        this.refreshDisplayedData();
 
         this.options = {
             legend: {
@@ -106,99 +99,104 @@ export class LineStatsComponent implements OnInit {
                 yAxes: [{
                     stacked: true
                 }]
+            },
+            animation: {
+                duration: 0
             }
         };
     }
 
-    getMinYear() {
-        return this.firstPossibleDate ? ""+this.firstPossibleDate.getFullYear() : "";
-    }
-
-    getMaxYear(): string {
-        return this.lastPossibleDate ? ""+this.lastPossibleDate.getFullYear() : "";
-    }
-
-    onDateRangeChange(event: Array<Date>) {
-        if (event[0]) {
-            this.firstDisplayedDate = event[0];
-        } else {
-            this.firstDisplayedDate = this.firstPossibleDate;
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['startDate'] || changes['endDate'] || changes['showDetails']) {
+            this.refreshDisplayedData();
         }
-        if (event[1]) {
-            this.lastDisplayedDate = event[1];
-        } else {
-            this.lastDisplayedDate = this.lastPossibleDate;
-        }
-
-        this.refreshDisplayedData();
     }
 
     private refreshDisplayedData() {
         this.passedData = [];
         for (const dataPoint of this.allPassedData) {
-            if (this.firstDisplayedDate <= dataPoint.x && dataPoint.x <= this.lastDisplayedDate) {
+            if (this.startDate <= dataPoint.x && dataPoint.x <= this.endDate) {
                 this.passedData.push(dataPoint)
             }
         }
         this.failedData = [];
         for (const dataPoint of this.allFailedData) {
-            if (this.firstDisplayedDate <= dataPoint.x && dataPoint.x <= this.lastDisplayedDate) {
+            if (this.startDate <= dataPoint.x && dataPoint.x <= this.endDate) {
                 this.failedData.push(dataPoint)
             }
         }
         this.undefinedData = [];
         for (const dataPoint of this.allUndefinedData) {
-            if (this.firstDisplayedDate <= dataPoint.x && dataPoint.x <= this.lastDisplayedDate) {
-                this.undefinedData.push(dataPoint)
+            if (this.startDate <= dataPoint.x && dataPoint.x <= this.endDate) {
+                this.showDetails ? this.undefinedData.push(dataPoint) : this.addPointToFailedData(dataPoint);
             }
         }
         this.disabledData = [];
         for (const dataPoint of this.allDisabledData) {
-            if (this.firstDisplayedDate <= dataPoint.x && dataPoint.x <= this.lastDisplayedDate) {
-                this.disabledData.push(dataPoint)
+            if (this.startDate <= dataPoint.x && dataPoint.x <= this.endDate) {
+                this.showDetails ? this.disabledData.push(dataPoint) : this.addPointToFailedData(dataPoint);
+
             }
         }
         this.skippedData = [];
         for (const dataPoint of this.allSkippedData) {
-            if (this.firstDisplayedDate <= dataPoint.x && dataPoint.x <= this.lastDisplayedDate) {
-                this.skippedData.push(dataPoint)
+            if (this.startDate <= dataPoint.x && dataPoint.x <= this.endDate) {
+                this.showDetails ? this.skippedData.push(dataPoint) : this.addPointToFailedData(dataPoint);
+
             }
         }
         this.data = this.createData();
     }
 
     private createData(): any {
-        return {
-            datasets: [
+        let datasets = [];
 
+        if (this.showDetails && this.dataFieldHasValues(this.disabledData)) {
+            datasets.push(
                 {
                     label: 'Disabled',
                     data: this.disabledData,
                     fill: true,
                     borderColor: "#c0bebc",
                     backgroundColor: "#c0bebc"
-                },
+                }
+            )
+        }
+        if (this.showDetails && this.dataFieldHasValues(this.skippedData)) {
+            datasets.push(
                 {
                     label: 'Skipped',
                     data: this.skippedData,
                     fill: true,
                     borderColor: "#aae8ff",
                     backgroundColor: "#aae8ff"
-                },
+                }
+            )
+        }
+        if (this.showDetails && this.dataFieldHasValues(this.undefinedData)) {
+            datasets.push(
                 {
                     label: 'Undefined',
                     data: this.undefinedData,
                     fill: true,
                     borderColor: "#FFCE56",
                     backgroundColor: "#ffce56"
-                },
+                }
+            )
+        }
+        if (this.dataFieldHasValues(this.passedData)) {
+            datasets.push(
                 {
                     label: 'Passed',
                     data: this.passedData,
                     fill: true,
                     borderColor: "#5cb85c",
                     backgroundColor: "#5cb85c"
-                },
+                }
+            );
+        }
+        if (this.dataFieldHasValues(this.failedData)) {
+            datasets.push(
                 {
                     label: 'Failed',
                     data: this.failedData,
@@ -206,11 +204,27 @@ export class LineStatsComponent implements OnInit {
                     borderColor: "#FF6384",
                     backgroundColor: "#ff6384"
                 }
-            ]
-        };
+            );
+        }
+
+        return {datasets: datasets};
+    }
+
+    dataFieldHasValues(dataPoints: DataPointModel[]): boolean {
+        for (const point of dataPoints) {
+            if (point.y > 0) {
+                return true
+            }
+        }
     }
 
     onMaxMinClick() {
         this.fullWidth = !this.fullWidth;
+    }
+
+    private addPointToFailedData(dataPoint: DataPointModel) {
+        let failedDataPointIndex = this.failedData.findIndex(value => {return dataPoint.x == value.x});
+        let failedDataPoint = this.failedData[failedDataPointIndex];
+        this.failedData[failedDataPointIndex] = new DataPointModel(failedDataPoint.x, failedDataPoint.y + dataPoint.y)
     }
 }
