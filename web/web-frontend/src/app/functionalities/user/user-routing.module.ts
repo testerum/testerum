85dbb6ecdeafec2365107d68032e535dbb0464e3
Marius Gradinaru <imariusgradinaru@gmail.com>
2019-02-25 18:43:26 +0100

    Small changes in menu.component.html

--- web/web-frontend/src/app/functionalities/user/user-routing.module.ts
+++ web/web-frontend/src/app/functionalities/user/user-routing.module.ts
@@ -1,13 +1,8 @@
 import {NgModule} from '@angular/core';
-
 import {Routes, RouterModule} from "@angular/router";
-import {SetupGuard} from "../../service/guards/setup.guard";
-import {FeedbackComponent } from "./feedback/feedback.component"
-
 
 const userRoutes: Routes = [
-    {
-    },
+    {},
 ];
 @NgModule({
     imports: [
