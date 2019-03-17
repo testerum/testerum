87a079710507bd4b7891dd123cb71358b92ba266
Cristian Mocanu <cvmocanu@gmail.com>
2019-03-17 21:07:29 +0100

    TM-908 [runner/expressions] easier-to-understand error message

--- common/common-expression-evaluator/src/main/kotlin/com/testerum/common/expression_evaluator/ExpressionEvaluator.kt
+++ common/common-expression-evaluator/src/main/kotlin/com/testerum/common/expression_evaluator/ExpressionEvaluator.kt
@@ -7,9 +7,13 @@ import com.testerum.common.expression_evaluator.helpers.impl.GenerateStringByReg
 import com.testerum.common.expression_evaluator.helpers.impl.UuidScriptingHelper
 import delight.nashornsandbox.NashornSandbox
 import delight.nashornsandbox.NashornSandboxes
+import jdk.nashorn.internal.runtime.ECMAException
+import org.slf4j.LoggerFactory
 import javax.script.Bindings
 
 object ExpressionEvaluator {
+
+    private val LOG = LoggerFactory.getLogger(ExpressionEvaluator::class.java)
 
     // todo: make these configurable
     private val helpers = listOf<ScriptingHelper>(
@@ -40,17 +44,27 @@ object ExpressionEvaluator {
 
         var enhancedExpression: String? = null
         try {
+            // todo: use sandbox.inject() for helpers
             enhancedExpression = enhanceExpression(expression)
 
             return sandbox.eval(enhancedExpression, bindings)
         } catch (e: Exception) {
-            val errorMessage = buildString {
-                append("failed to evaluate expression [").append(expression).append("]")
-
-                if (enhancedExpression != null) {
-                    append(", enhancedExpression=[").append(enhancedExpression).append("]")
-                }
+            val ecmaException = e.selfOrCauseOfType<ECMAException>()
+            val errorMessage = if (ecmaException != null) {
+                ecmaException.message
+            } else {
+                e.message
             }
+
+            LOG.info(
+                    buildString {
+                        append("failed to evaluate expression [").append(expression).append("]")
+
+                        if (enhancedExpression != null) {
+                            append(", enhancedExpression=[\n").append(enhancedExpression).append("\n]")
+                        }
+                    }
+            )
 
             throw RuntimeException(errorMessage, e)
         }
@@ -67,4 +81,17 @@ object ExpressionEvaluator {
         append(expression)
     }
 
+    private inline fun <reified T> Throwable.selfOrCauseOfType(): T? {
+        var exception: Throwable? = this
+
+        do {
+            if (exception is T) {
+                return exception
+            }
+
+            exception = exception?.cause
+        } while (exception != null)
+
+        return null
+    }
 }
