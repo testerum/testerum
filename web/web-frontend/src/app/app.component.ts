46d9c84226f679f56916804f788546caefa42c19
Ionut Pruteanu <ionut.pruteanu@gmail.com>
2019-03-19 13:20:34 +0100

    TM-910 update angular dependencies

--- web/web-frontend/src/app/app.component.ts
+++ web/web-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, ComponentFactoryResolver, ViewContainerRef} from '@angular/core';
 import {MessageService} from "./service/message.service";
+import {setTheme} from "ngx-bootstrap";
 
 @Component({
   moduleId:module.id,
@@ -13,6 +14,8 @@ export class AppComponent  {
     constructor(messageService: MessageService,
                 private viewContainerRef: ViewContainerRef,
                 private componentFactoryResolver: ComponentFactoryResolver) {
+        setTheme('bs3');
+
         AppComponent.rootViewContainerRef = viewContainerRef;
 
         messageService.init();
