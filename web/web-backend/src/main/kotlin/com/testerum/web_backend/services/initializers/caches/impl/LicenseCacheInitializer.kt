f99069e53f993f5f9084409367d34b3715f1a5a9
Cristian Mocanu <cvmocanu@gmail.com>
2019-04-07 11:57:00 +0200

    LicenseCacheInitializer: remove unused field

--- web/web-backend/src/main/kotlin/com/testerum/web_backend/services/initializers/caches/impl/LicenseCacheInitializer.kt
+++ web/web-backend/src/main/kotlin/com/testerum/web_backend/services/initializers/caches/impl/LicenseCacheInitializer.kt
@@ -2,14 +2,9 @@ package com.testerum.web_backend.services.initializers.caches.impl
 
 import com.testerum.cloud_client.licenses.cache.LicensesCache
 import com.testerum.web_backend.services.dirs.FrontendDirs
-import org.slf4j.LoggerFactory
 
 class LicenseCacheInitializer(private val frontendDirs: FrontendDirs,
                               private val licensesCache: LicensesCache) {
-
-    companion object {
-        private val LOG = LoggerFactory.getLogger(LicenseCacheInitializer::class.java)
-    }
 
     fun initialize() {
         val licensesDir = frontendDirs.getLicensesDir()
