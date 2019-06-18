bae13d98c8e16ad78be171c6aa6a0dfa0c8a138a
Cristian Mocanu <cvmocanu@gmail.com>
2019-06-18 15:51:33 +0200

    selenium-steps: support Microsoft Internet Explorer

--- steps/selenium-steps/src/main/kotlin/selenium_steps_support/service/webdriver_factory/WebDriverFactories.kt
+++ steps/selenium-steps/src/main/kotlin/selenium_steps_support/service/webdriver_factory/WebDriverFactories.kt
@@ -4,15 +4,17 @@ import com.testerum.api.test_context.settings.model.SeleniumBrowserType
 import selenium_steps_support.service.webdriver_factory.chrome.ChromeWebDriverFactory
 import selenium_steps_support.service.webdriver_factory.chrome.EdgeWebDriverFactory
 import selenium_steps_support.service.webdriver_factory.chrome.FirefoxWebDriverFactory
+import selenium_steps_support.service.webdriver_factory.chrome.InternetExplorerWebDriverFactory
 import selenium_steps_support.service.webdriver_factory.chrome.OperaWebDriverFactory
 
 object WebDriverFactories {
 
     fun getWebDriverFactory(browserType: SeleniumBrowserType): WebDriverFactory = when (browserType) {
-        SeleniumBrowserType.CHROME  -> ChromeWebDriverFactory
-        SeleniumBrowserType.FIREFOX -> FirefoxWebDriverFactory
-        SeleniumBrowserType.OPERA   -> OperaWebDriverFactory
-        SeleniumBrowserType.EDGE    -> EdgeWebDriverFactory
+        SeleniumBrowserType.CHROME             -> ChromeWebDriverFactory
+        SeleniumBrowserType.FIREFOX            -> FirefoxWebDriverFactory
+        SeleniumBrowserType.OPERA              -> OperaWebDriverFactory
+        SeleniumBrowserType.EDGE               -> EdgeWebDriverFactory
+        SeleniumBrowserType.INTERNET_EXPLORER  -> InternetExplorerWebDriverFactory
         else -> throw IllegalArgumentException("not yet implemented") // todo: implement for other browsers also}
     }
 
---
+++ steps/selenium-steps/src/main/kotlin/selenium_steps_support/service/webdriver_factory/chrome/InternetExplorerWebDriverFactory.kt
@@ -0,0 +1,40 @@
+package selenium_steps_support.service.webdriver_factory.chrome
+
+import com.testerum.api.test_context.settings.model.SeleniumDriverSettingValue
+import com.testerum.model.selenium.SeleniumDriversByBrowser
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.ie.InternetExplorerDriver
+import org.openqa.selenium.ie.InternetExplorerOptions
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import selenium_steps_support.service.webdriver_factory.WebDriverFactory
+import selenium_steps_support.utils.SeleniumStepsDirs
+import java.nio.file.Path as JavaPath
+
+object InternetExplorerWebDriverFactory : WebDriverFactory {
+
+    private val LOG: Logger = LoggerFactory.getLogger(InternetExplorerWebDriverFactory::class.java)
+
+    override fun createWebDriver(config: SeleniumDriverSettingValue,
+                                 driversByBrowser: SeleniumDriversByBrowser): WebDriver {
+        val driverInfo = driversByBrowser.getDriverInfoByBrowserAndDriverVersion(
+                browserType = config.browserType,
+                driverVersion = config.driverVersion
+        ) ?: throw RuntimeException("could not find a Selenium driver for browserType=[${config.browserType}] and driverVersion=[${config.driverVersion}]")
+
+        val driverBinaryPath: JavaPath = SeleniumStepsDirs.getSeleniumDriversDir()
+                .resolve(driverInfo.relativePath)
+                .toAbsolutePath()
+                .normalize()
+
+        LOG.info("using Selenium driver [$driverBinaryPath]")
+
+        // todo: this is nasty: it's a global variable preventing us from using different drivers (e.g. different Chrome versions) at the same time ==> find a better way)
+        System.setProperty("webdriver.ie.driver", driverBinaryPath.toAbsolutePath().toString())
+
+        val options = InternetExplorerOptions()
+
+        return InternetExplorerDriver(options)
+    }
+
+}
