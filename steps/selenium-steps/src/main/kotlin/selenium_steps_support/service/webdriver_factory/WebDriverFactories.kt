62f8edba1210a1fe7042f48d144d76a136fff4eb
Cristian Mocanu <cvmocanu@gmail.com>
2019-06-18 15:26:09 +0200

    selenium-steps: support Microsoft Edge

--- steps/selenium-steps/src/main/kotlin/selenium_steps_support/service/webdriver_factory/WebDriverFactories.kt
+++ steps/selenium-steps/src/main/kotlin/selenium_steps_support/service/webdriver_factory/WebDriverFactories.kt
@@ -2,15 +2,17 @@ package selenium_steps_support.service.webdriver_factory
 
 import com.testerum.api.test_context.settings.model.SeleniumBrowserType
 import selenium_steps_support.service.webdriver_factory.chrome.ChromeWebDriverFactory
+import selenium_steps_support.service.webdriver_factory.chrome.EdgeWebDriverFactory
 import selenium_steps_support.service.webdriver_factory.chrome.FirefoxWebDriverFactory
 import selenium_steps_support.service.webdriver_factory.chrome.OperaWebDriverFactory
 
 object WebDriverFactories {
 
     fun getWebDriverFactory(browserType: SeleniumBrowserType): WebDriverFactory = when (browserType) {
-        SeleniumBrowserType.CHROME -> ChromeWebDriverFactory
+        SeleniumBrowserType.CHROME  -> ChromeWebDriverFactory
         SeleniumBrowserType.FIREFOX -> FirefoxWebDriverFactory
-        SeleniumBrowserType.OPERA -> OperaWebDriverFactory
+        SeleniumBrowserType.OPERA   -> OperaWebDriverFactory
+        SeleniumBrowserType.EDGE    -> EdgeWebDriverFactory
         else -> throw IllegalArgumentException("not yet implemented") // todo: implement for other browsers also}
     }
 
---
+++ steps/selenium-steps/src/main/kotlin/selenium_steps_support/service/webdriver_factory/chrome/EdgeWebDriverFactory.kt
@@ -0,0 +1,40 @@
+package selenium_steps_support.service.webdriver_factory.chrome
+
+import com.testerum.api.test_context.settings.model.SeleniumDriverSettingValue
+import com.testerum.model.selenium.SeleniumDriversByBrowser
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.edge.EdgeDriver
+import org.openqa.selenium.edge.EdgeOptions
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import selenium_steps_support.service.webdriver_factory.WebDriverFactory
+import selenium_steps_support.utils.SeleniumStepsDirs
+import java.nio.file.Path as JavaPath
+
+object EdgeWebDriverFactory : WebDriverFactory {
+
+    private val LOG: Logger = LoggerFactory.getLogger(EdgeWebDriverFactory::class.java)
+
+    override fun createWebDriver(config: SeleniumDriverSettingValue,
+                                 driversByBrowser: SeleniumDriversByBrowser): WebDriver {
+        val driverInfo = driversByBrowser.getDriverInfoByBrowserAndDriverVersion(
+                browserType = config.browserType,
+                driverVersion = config.driverVersion
+        ) ?: throw RuntimeException("could not find a Selenium driver for browserType=[${config.browserType}] and driverVersion=[${config.driverVersion}]")
+
+        val driverBinaryPath: JavaPath = SeleniumStepsDirs.getSeleniumDriversDir()
+                .resolve(driverInfo.relativePath)
+                .toAbsolutePath()
+                .normalize()
+
+        LOG.info("using Selenium driver [$driverBinaryPath]")
+
+        // todo: this is nasty: it's a global variable preventing us from using different drivers (e.g. different Chrome versions) at the same time ==> find a better way)
+        System.setProperty("webdriver.edge.driver", driverBinaryPath.toAbsolutePath().toString())
+
+        val options = EdgeOptions()
+
+        return EdgeDriver(options)
+    }
+
+}
