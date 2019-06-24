ebd3d20518ed40505d04e18fdfe02be06df9454d
Cristian Mocanu <cvmocanu@gmail.com>
2019-06-24 23:19:15 +0200

    selenium-steps: add step to switch the context to another window

--- steps/selenium-steps/src/main/kotlin/selenium/actions/contexts/WebDriverContextSwitchSteps.kt
+++ steps/selenium-steps/src/main/kotlin/selenium/actions/contexts/WebDriverContextSwitchSteps.kt
@@ -3,6 +3,7 @@ package selenium.actions.contexts
 import com.testerum.api.annotations.steps.When
 import selenium_steps_support.service.frame_switchers.FrameSwitchers
 import selenium_steps_support.service.module_di.SeleniumModuleServiceLocator
+import selenium_steps_support.service.text_match.TextMatcherService
 import selenium_steps_support.service.webdriver_manager.WebDriverManager
 
 class WebDriverContextSwitchSteps {
@@ -25,6 +26,28 @@ class WebDriverContextSwitchSteps {
     )
     fun switchToTopFrame() {
         webDriverManager.switchCurrentWebDriverToTop()
+    }
+
+    @When(
+            value = "I switch the context to the window with the title <<textMatchExpression>>"
+    )
+    fun switchToWindow(textMatchExpression: String) {
+        webDriverManager.executeWebDriverStep { driver ->
+            val originalWindowHandle = driver.windowHandle
+
+            for (windowHandle in driver.windowHandles) {
+                driver.switchTo().window(windowHandle)
+
+                if (TextMatcherService.matches(textMatchExpression, driver.title)) {
+                    return@executeWebDriverStep
+                }
+            }
+
+            // nothing matched => restore original context & throw error
+            driver.switchTo().window(originalWindowHandle)
+
+            throw RuntimeException("could not find any window that matches the text expression [$textMatchExpression]")
+        }
     }
 
 }
