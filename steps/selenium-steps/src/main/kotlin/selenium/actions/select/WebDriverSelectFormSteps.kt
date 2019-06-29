32cda03f342f7411b3ac461cc1247a5b87bf924e
Ionut Pruteanu <ionut.pruteanu@gmail.com>
2019-06-30 01:44:27 +0200

    add missing step in selenium  - remove selection from select element

--- steps/selenium-steps/src/main/kotlin/selenium/actions/select/WebDriverSelectFormSteps.kt
+++ steps/selenium-steps/src/main/kotlin/selenium/actions/select/WebDriverSelectFormSteps.kt
@@ -21,7 +21,7 @@ class WebDriverSelectFormSteps {
                           "Option locators provide different ways of specifying a select element (e.g., label=, value=, index=).\n" +
                           "If no option locator prefix is provided, a match on the label will be attempted."
     )
-    fun addSelectionToMultiSelectByText (
+    fun selectItem (
             @Param(
                     description = "This parameter provide different ways of specifying the element to select (e.g., label=, value=, index=).\n" +
                                   "If no option locator prefix is provided, a match on the label will be attempted."
@@ -34,6 +34,61 @@ class WebDriverSelectFormSteps {
     ) {
         logger.info(
                 "select an item in drop-down or multi-select element\n" +
+                        "--------------------------------------\n" +
+                        "elementLocator : $elementLocator\n" +
+                        "optionLocator  : $optionLocator\n" +
+                        "\n"
+        )
+
+        webDriverManager.waitForElementPresent(elementLocator)
+        webDriverManager.executeWebDriverStep { driver ->
+            val field: WebElement = ElementLocatorService.locateElement(driver, elementLocator)
+                    ?: throw AssertionError("the field [$elementLocator] should be present on the page, but is not")
+
+            val select = Select(field)
+            if (optionLocator.startsWith("label=")) {
+                val itemLabel = optionLocator.removePrefix("label=")
+                select.selectByVisibleText(itemLabel)
+                return@executeWebDriverStep;
+            }
+            if (optionLocator.startsWith("value=")) {
+                val itemValue = optionLocator.removePrefix("value=")
+                select.selectByValue(itemValue)
+                return@executeWebDriverStep;
+            }
+            if (optionLocator.startsWith("index=")) {
+                val itemIndexAsString = optionLocator.removePrefix("index=")
+                try {
+                    val itemIndex = itemIndexAsString.toInt()
+                    select.selectByIndex(itemIndex)
+                } catch (e: Exception) {
+                    throw AssertionError("the index value [$elementLocator] should be a number")
+                }
+                return@executeWebDriverStep;
+            }
+            select.selectByVisibleText(optionLocator)
+        }
+    }
+
+    @When(
+            value = "I deselect <<optionLocator>> from the select element <<elementLocator>>",
+            description = "Deselect an element from a drop-down or multi-select using an option locator.\n" +
+                          "Option locators provide different ways of specifying a select element (e.g., label=, value=, index=).\n" +
+                          "If no option locator prefix is provided, a match on the label will be attempted."
+    )
+    fun deselectSelectItem (
+            @Param(
+                    description = "This parameter provide different ways of specifying the element to select (e.g., label=, value=, index=).\n" +
+                                  "If no option locator prefix is provided, a match on the label will be attempted."
+            )
+            optionLocator: String,
+            @Param(
+                    description = SeleniumSharedDescriptions.ELEMENT_LOCATOR_DESCRIPTION
+            )
+            elementLocator: String
+    ) {
+        logger.info(
+                "deselect an item in drop-down or multi-select element\n" +
                         "--------------------------------------\n" +
                         "elementLocator : $elementLocator\n" +
                         "optionLocator  : $optionLocator\n" +
