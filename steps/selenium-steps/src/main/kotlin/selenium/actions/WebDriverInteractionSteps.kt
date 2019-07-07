3142b31104ed0782171f534cec02e00d1b5cc99f
Ionut Pruteanu <ionut.pruteanu@gmail.com>
2019-07-07 19:11:57 +0200

    add missing step in selenium  - steps for click element at position

--- steps/selenium-steps/src/main/kotlin/selenium/actions/WebDriverInteractionSteps.kt
+++ steps/selenium-steps/src/main/kotlin/selenium/actions/WebDriverInteractionSteps.kt
@@ -11,10 +11,6 @@ import selenium_steps_support.service.elem_locators.ElementLocatorService
 import selenium_steps_support.service.module_di.SeleniumModuleServiceLocator
 import selenium_steps_support.service.webdriver_manager.WebDriverManager
 
-
-
-
-
 class WebDriverInteractionSteps {
 
     private val logger = TesterumServiceLocator.getTesterumLogger()
@@ -51,6 +47,60 @@ class WebDriverInteractionSteps {
         }
     }
 
+//----------------------------------------------------------------------------------------------------------------------
+    @When(
+            value = "I click the element <<elementLocator>> at <<position>>",
+            description = "Clicks at the specified position (X, Y) on a target element (e.g., a link, button, checkbox, or radio button).\n" +
+                    "The coordinates are relative to the target element (e.g., 0,0 is the top left corner of the element)."
+    )
+    fun clickAt(
+            @Param(
+                    description = SeleniumSharedDescriptions.ELEMENT_LOCATOR_DESCRIPTION
+            )
+            elementLocator: String,
+            @Param(
+                    description = "Specifies the x,y position (i.e. - 10,20) of the mouse event relative to the element returned by the locator."
+            )
+            position: String
+    ) {
+        logger.info(
+                "clicking at\n" +
+                "-----------\n" +
+                "elementLocator : $elementLocator\n" +
+                "position : $position\n" +
+                "\n"
+        )
+
+    val split = position.split(",")
+    if(split.size != 2) {
+        throw AssertionError("the position parameter [$position] is not a valid. The parameter format should be two numbers separated by comma. (i.e. - 10,20)")
+    }
+
+    val x = try {
+        split[0].toInt()
+    } catch (e: NumberFormatException) {
+        throw AssertionError("the position parameter [$position] is not a valid. The parameter format should be two numbers separated by comma. (i.e. - 10,20)")
+    }
+    val y = try {
+        split[1].toInt()
+    } catch (e: NumberFormatException) {
+        throw AssertionError("the position parameter [$position] is not a valid. The parameter format should be two numbers separated by comma. (i.e. - 10,20)")
+    }
+
+    webDriverManager.waitForElementPresent(elementLocator)
+        webDriverManager.executeWebDriverStep { driver ->
+            val element: WebElement = ElementLocatorService.locateElement(driver, elementLocator)
+                    ?: throw AssertionError("the element [$elementLocator] should be present on the page, but is not")
+
+            Actions(driver)
+                    .moveToElement(element, x, y)
+                    .click()
+                    .build()
+                    .perform()
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
     @When(
             value = "I double-click the element <<elementLocator>>",
             description = "Simulates a mouse double-click on the given element."
@@ -81,6 +131,7 @@ class WebDriverInteractionSteps {
         }
     }
 
+//----------------------------------------------------------------------------------------------------------------------
     @When(
             value = "I scroll element <<elementLocator>> into the view",
             description = "Changes the scroll bar position to bring the element into the view port of the browser"
