79d06c91a4cf8a6a8a440f0f36f968d9bc5cbc7a
Ionut Pruteanu <ionut.pruteanu@gmail.com>
2019-07-11 10:42:36 +0200

    add missing step in selenium  - small improvements

--- steps/selenium-steps/src/main/kotlin/selenium/actions/util/WebDriverExpressionSteps.kt
+++ steps/selenium-steps/src/main/kotlin/selenium/actions/util/WebDriverExpressionSteps.kt
@@ -31,7 +31,11 @@ class WebDriverExpressionSteps {
 
     @When(
             value = "I execute the WebDriver JS script <<script>>",
-            description = ""
+            description = "Executes a custom JavaScript script that has access to the WebDriver and all the variables in the test scope.\n\n" +
+                    "This script is not executed in the browser context.\n\n" +
+                    "In this scritp context you have access to the following variables:\n\n" +
+                    "  - ``webDriver`` - this is a WebDriver instance and allows you to execute any Selenium action you want (e.g. ``webDriver.findElement(By.id('submit').click();``).\n" +
+                    "  - ``testVariables`` - this is a Map with all the variables defined in this test.\n"
     )
     fun evaluateWebDriverExpresion(
             @Param(
