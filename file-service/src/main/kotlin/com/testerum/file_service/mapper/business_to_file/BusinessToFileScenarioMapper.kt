50b613acaf54e33ec5e54bddd68113391ddfa6ee
Cristian Mocanu <cvmocanu@gmail.com>
2019-08-06 18:28:14 +0200

    scenarios: implement mapping between the business and file models

---
+++ file-service/src/main/kotlin/com/testerum/file_service/mapper/business_to_file/BusinessToFileScenarioMapper.kt
@@ -0,0 +1,17 @@
+package com.testerum.file_service.mapper.business_to_file
+
+import com.testerum.model.test.scenario.Scenario
+import com.testerum.test_file_format.testdef.scenarios.FileScenario
+
+class BusinessToFileScenarioMapper(private val businessToFileScenarioParamMapper: BusinessToFileScenarioParamMapper) {
+
+    fun mapScenarios(scenarios: List<Scenario>): List<FileScenario> = scenarios.map { mapScenario(it) }
+
+    fun mapScenario(scenario: Scenario): FileScenario {
+        return FileScenario(
+                name = scenario.name,
+                params = businessToFileScenarioParamMapper.mapParams(scenario.params)
+        )
+    }
+
+}
