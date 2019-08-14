105e9aa124f63a921e6542a0bbef144a9b174357
Cristian Mocanu <cvmocanu@gmail.com>
2019-08-14 23:36:10 +0200

    bugfix: incorrect event keys for scenario and parametrized tests

--- web/web-backend/src/main/kotlin/com/testerum/web_backend/services/runner/execution/TestRunnerEventParser.kt
+++ web/web-backend/src/main/kotlin/com/testerum/web_backend/services/runner/execution/TestRunnerEventParser.kt
@@ -2,7 +2,11 @@ package com.testerum.web_backend.services.runner.execution
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.testerum.runner.events.model.ParametrizedTestEndEvent
+import com.testerum.runner.events.model.ParametrizedTestStartEvent
 import com.testerum.runner.events.model.RunnerEvent
+import com.testerum.runner.events.model.ScenarioEndEvent
+import com.testerum.runner.events.model.ScenarioStartEvent
 import com.testerum.runner.events.model.StepEndEvent
 import com.testerum.runner.events.model.StepStartEvent
 import com.testerum.runner.events.model.SuiteEndEvent
@@ -60,12 +64,16 @@ class TestRunnerEventParser(private val jsonObjectMapper: ObjectMapper,
 
     private fun processTestRunnerEvent(event: RunnerEvent) {
         when (event) {
-            is SuiteStartEvent -> { eventKeysStack.clear(); eventKeysStack.add(event.eventKey); }
-            is TestStartEvent  -> eventKeysStack.add(event.eventKey)
-            is StepStartEvent  -> eventKeysStack.add(event.eventKey)
-            is SuiteEndEvent   -> eventKeysStack.removeAt(eventKeysStack.lastIndex)
-            is TestEndEvent    -> eventKeysStack.removeAt(eventKeysStack.lastIndex)
-            is StepEndEvent    -> eventKeysStack.removeAt(eventKeysStack.lastIndex)
+            is SuiteStartEvent            -> { eventKeysStack.clear(); eventKeysStack.add(event.eventKey); }
+            is TestStartEvent             -> eventKeysStack.add(event.eventKey)
+            is ParametrizedTestStartEvent -> eventKeysStack.add(event.eventKey)
+            is ScenarioStartEvent         -> eventKeysStack.add(event.eventKey)
+            is StepStartEvent             -> eventKeysStack.add(event.eventKey)
+            is SuiteEndEvent              -> eventKeysStack.removeAt(eventKeysStack.lastIndex)
+            is TestEndEvent               -> eventKeysStack.removeAt(eventKeysStack.lastIndex)
+            is ParametrizedTestEndEvent   -> eventKeysStack.removeAt(eventKeysStack.lastIndex)
+            is ScenarioEndEvent           -> eventKeysStack.removeAt(eventKeysStack.lastIndex)
+            is StepEndEvent               -> eventKeysStack.removeAt(eventKeysStack.lastIndex)
         }
 
         val eventToProcess: RunnerEvent = if (event is TextLogEvent) {
