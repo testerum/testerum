106785dda03968858b381460da388d26ebcbe0d1
Cristian Mocanu <cvmocanu@gmail.com>
2019-09-23 19:26:39 +0200

    integration-tests: take selenium screenshots

--- integration-tests/jenkins/3-execute-tests.sh
+++ integration-tests/jenkins/3-execute-tests.sh
@@ -4,4 +4,5 @@ exec "${WORKSPACE}/testerum/runner/bin/testerum-runner.sh" \
   --repository-directory "${WORKSPACE}/integration-tests/tests" \
   --setting testerum.selenium.driver.browserType=CHROME \
   --setting testerum.selenium.driver.headless=true \
+  --setting testerum.selenium.takeScreenshotAfterEachStep=true \
   --managed-reports-directory /var/www/downloads.testerum.com/reports
