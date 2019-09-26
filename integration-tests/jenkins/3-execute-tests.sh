99f129080616a885ad0f23edffee2ae66a2fd991
Cristian Mocanu <cvmocanu@gmail.com>
2019-09-26 21:57:02 +0200

    workaround: integration tests: disabling screenshots...
    ...because we can't take them while an alert/confirm/prompt box is open.
    See "TM-1252 [selenium] cannot take screenshot if an alert if open"

--- integration-tests/jenkins/3-execute-tests.sh
+++ integration-tests/jenkins/3-execute-tests.sh
@@ -4,6 +4,6 @@ exec "${WORKSPACE}/testerum/runner/bin/testerum-runner.sh" \
   --repository-directory "${WORKSPACE}/integration-tests/tests" \
   --setting testerum.selenium.driver.browserType=CHROME \
   --setting testerum.selenium.driver.headless=true \
-  --setting testerum.selenium.takeScreenshotAfterEachStep=true \
+  --setting testerum.selenium.takeScreenshotAfterEachStep=false \
   --report=CONSOLE_DEBUG \
   --managed-reports-directory /var/www/downloads.testerum.com/reports
