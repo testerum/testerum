f3bb35169533903eceb2f02ee67ab1ad1308a91a
Cristian Mocanu <cvmocanu@gmail.com>
2020-05-06 21:06:53 +0200

    runner reports: events aggregation: count also scenarios as test executions

--- testerum-backend/testerum-runner/testerum-runner-api/src/main/kotlin/com/testerum/runner/statistics_model/events_aggregators/StatsEventsCountByStatusTestAggregator.kt
+++ testerum-backend/testerum-runner/testerum-runner-api/src/main/kotlin/com/testerum/runner/statistics_model/events_aggregators/StatsEventsCountByStatusTestAggregator.kt
@@ -1,22 +1,31 @@
 package com.testerum.runner.statistics_model.events_aggregators
 
-import com.testerum_api.testerum_steps_api.test_context.ExecutionStatus
 import com.testerum.common_stats.Aggregator
 import com.testerum.common_stats.GroupingAggregator
 import com.testerum.common_stats.count.CountAggregator
 import com.testerum.runner.events.model.RunnerEvent
+import com.testerum.runner.events.model.ScenarioEndEvent
 import com.testerum.runner.events.model.TestEndEvent
 import com.testerum.runner.statistics_model.StatsCountByStatus
+import com.testerum_api.testerum_steps_api.test_context.ExecutionStatus
 
 class StatsEventsCountByStatusTestAggregator : Aggregator<RunnerEvent, StatsCountByStatus> {
 
-    private val aggregator = GroupingAggregator<TestEndEvent, ExecutionStatus, Long>(
-            extractKey = { testEndEvent -> testEndEvent.status },
+    private val aggregator = GroupingAggregator<RunnerEvent, ExecutionStatus, Long>(
+            extractKey = { event ->
+                if (event is TestEndEvent) {
+                    event.status
+                } else if (event is ScenarioEndEvent) {
+                    event.status
+                } else {
+                    throw IllegalArgumentException("unexpected event type [${event::class.java}]")
+                }
+            },
             createValueAggregator = { CountAggregator() }
     )
 
     override fun aggregate(event: RunnerEvent) {
-        if (event is TestEndEvent) {
+        if (event is TestEndEvent || event is ScenarioEndEvent) {
             aggregator.aggregate(event)
         }
     }
