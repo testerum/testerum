f50e91f089618f8227cb53027d65ad72e5319a99
Ionut Pruteanu <ionut.pruteneau@ah.nl>
2020-10-29 02:36:07 +0100

    fix fontawesome icon issues after upgrade

--- web/web-frontend/src/app/generic/components/info-icon/info-icon.component.ts
+++ web/web-frontend/src/app/generic/components/info-icon/info-icon.component.ts
@@ -18,24 +18,32 @@ export class InfoIconComponent {
     @Input() height: string;
     @Input() iconFontSize: string = '1.3em';
 
-    @ViewChild(OverlayPanel, { static: true }) overlayPanel: OverlayPanel ;
+    @ViewChild(OverlayPanel, { static: true }) overlayPanel: OverlayPanel;
     @ViewChild("actualTarget", { static: true }) actualTargetElement: ElementRef;
 
-    private shouldShow: boolean;
+    private isVisible: boolean = false;
+    private shouldHide: boolean = false;
     constructor(private cd: ChangeDetectorRef) {}
 
     show(event: any) {
-        this.shouldShow = true;
-        this.overlayPanel.show(event, this.actualTargetElement.nativeElement);
+        this.shouldHide = false;
+
+        if (!this.isVisible) {
+            this.isVisible = true;
+            this.overlayPanel.show(event, this.actualTargetElement.nativeElement);
+        }
+
     }
 
     waitAndHide(event: any) {
-        this.shouldShow = false;
+        this.shouldHide = true;
 
         (async () => {
             await FlowUtil.delay(250);
-            if (!this.shouldShow) {
+            if (this.isVisible && this.shouldHide) {
                 this.overlayPanel.hide();
+                this.isVisible = false;
+
                 this.refresh();
             }
         })();
