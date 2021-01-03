efa2019cb7880f150dae89dd9e92d6891a37576b
Cristian Mocanu <cvmocanu@gmail.com>
2021-01-03 16:41:59 +0100

    integration-tests: use the latest version of Chrome

--- integration-tests/jenkins/4-execute-tests.sh
+++ integration-tests/jenkins/4-execute-tests.sh
@@ -3,7 +3,6 @@
 exec "${WORKSPACE}/testerum/runner/bin/testerum-runner.sh" \
   --repository-directory "${WORKSPACE}/integration-tests/tests" \
   --setting testerum.selenium.driver.browserType=CHROME \
-  --setting testerum.selenium.driver.driverVersion=79.0.3945.36 \
   --setting testerum.selenium.driver.headless=true \
   --setting testerum.selenium.takeScreenshotAfterEachStep=false \
   --report CONSOLE_DEBUG \
