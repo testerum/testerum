f9796ae3b599778f4dfe896741df3ee06d9d22b9
Marius Gradinaru <imariusgradinaru@gmail.com>
2021-05-23 22:41:31 +0200

    Jenkins scripts added to automatically execute the tests from CI/CD

---
+++ testerum-ui-tests/jenkins/4-execute-tests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+exec "${WORKSPACE}/testerum/runner/bin/testerum-runner.sh" \
+  --repository-directory "${WORKSPACE}/testerum-ui-tests/tests" \
+  --setting testerum.selenium.driver.browserType=CHROME \
+  --setting testerum.selenium.driver.headless=true \
+  --setting testerum.selenium.takeScreenshotAfterEachStep=false \
+  --report CONSOLE_DEBUG \
+  --managed-reports-directory /var/www/downloads.testerum.com/reports
